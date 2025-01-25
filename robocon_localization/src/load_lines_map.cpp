f09988eef35532da763776b5713c4914a8ca9130
Robot <zhangwanjie@126.com>
2025-01-25 22:18:08 +0800

    save lines_maps to files

--- robocon_localization/src/load_lines_map.cpp
+++ robocon_localization/src/load_lines_map.cpp
@@ -10,9 +10,23 @@ int main(int argc, char** argv) {
 
     LinesMap lines_map;
     lines_map.loadImage(imagePath);
-    cv::Mat resultImage = lines_map.getLinesMap();
+    cv::Mat whiteImage = lines_map.getLinesMap();
+    cv::Mat blueImage = lines_map.getBlueMap();
+    cv::Mat redImage = lines_map.getRedMap();
 
-    cv::imshow("lines", resultImage);
+    // 获取用户主目录路径
+    const char* homeDir = getenv("HOME");
+    if (homeDir != nullptr) {
+        std::string savePath(homeDir);
+        // 保存为无损PNG格式
+        cv::imwrite(savePath + "/white_lines.png", whiteImage);
+        cv::imwrite(savePath + "/blue_lines.png", blueImage);
+        cv::imwrite(savePath + "/red_lines.png", redImage);
+    }
+
+    cv::imshow("lines", whiteImage);
+    cv::imshow("blue map", blueImage);
+    cv::imshow("red map", redImage);
     cv::waitKey(0);
     return 0;
 }
